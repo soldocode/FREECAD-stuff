6a6e659eb5e7d24ec4900517f4560b3939be1b25
Riccardo Soldini <soldocode@users.noreply.github.com>
2020-03-24 19:04:53 +0100

    Added get_branches_from_face function

--- Macro/reversE-unFold.py
+++ Macro/reversE-unFold.py
@@ -18,6 +18,56 @@ from MeFreeCADClasses import *
 from PySide import QtGui
 
 
+
+def get_branches_from_face(face_up_from,map,couples):
+
+	next_calls=[]
+	branches=[]
+	face_down=None
+	for c in couples:
+		if face_up_from==c[0]:
+			face_down_from=c[1]
+			couple=c
+		if face_up_from==c[1]:
+			face_down_from=c[0]
+			couple=c
+	#print ('...lookin for face up from ',face_up_from)
+
+	links_up=map[face_up_from]
+	#print ('links up:',links_up)
+	for geo_up_from in links_up:
+		for p in couples:
+			face_up_to=links_up[geo_up_from][0]
+			if face_up_to in p:
+				#print (face_up_to,' found in',p,' with geo id ',geo_up_from)
+				#print ('...lookin for face down from ',face_down_from)
+				links_down=map[face_down_from]
+				#print('links_down:',links_down)
+				for geo_down_from in links_down:
+					face_down_to=links_down[geo_down_from][0]
+					if face_down_to in p:
+						#print (face_down_to,' found in',p,' with geo id ',geo_down_from)
+						branches.append(dict(face_up_from=face_up_from,
+											 geo_up_from=geo_up_from,
+											 face_up_to=face_up_to,
+											 geo_up_to=links_up[geo_up_from][1],
+											 face_down_from=face_down_from,
+											 geo_down_from=geo_down_from,
+											 face_down_to=face_down_to,
+											 geo_down_to=links_down[geo_down_from][1]))
+						next_calls.append(face_up_to)
+						#branches+=get_branches_from_face(face_up_to,map,c)
+
+	c=list(couples)
+	c.remove(couple)
+	for n in next_calls:
+		branches+=get_branches_from_face(n,map,c)
+
+	return branches
+
+
+
+
 print ("Let's begin...")
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -31,9 +81,10 @@ OGG._getAnyBends()
 
 sheet_tree=FCTreeSheet(OGG)
 
+planes=[]
 
-for t in OGG.FacesMap:
-	print(t,'-->',OGG.FacesMap[t])
+#for t in OGG.FacesMap:
+	#print(t,'-->',OGG.FacesMap[t])
 
 root=list(OGG.FacesMap.keys())[0]
 #area=OGG.Faces[root].Area
@@ -69,6 +120,17 @@ while (not tree_filled) and (id_key<len(af)):
 		f2=OGG.Faces[idf2]
 		dist=round(f1.distToShape(f2)[0],1)
 		if dist==OGG.Thk:
-			print('thk:',dist,' ->',idf1,'-',idf2)
+			#print('thk:',dist,' ->',idf1,'-',idf2)
+			planes.append([idf1,idf2])
 	id_key+=1
-#thk=round(f1.distToShape(f2)[0],1)
+
+
+#bb=OGG.BendedFaces[0]
+
+print('planes:',planes)
+print('bends:',OGG.BendedFaces)
+planes+=OGG.BendedFaces
+
+branches=[]
+branches=get_branches_from_face(planes[0][0],OGG.FacesMap['Map'],planes)
+print (branches)
