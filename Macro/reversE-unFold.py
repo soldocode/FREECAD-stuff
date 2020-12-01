1d0253520f5c2938408573f1b2c80b5142daf74f
Riccardo Soldini <soldocode@users.noreply.github.com>
2020-12-01 19:40:15 +0100

    Updated reversE-unFold.py

--- Macro/reversE-unFold.py
+++ Macro/reversE-unFold.py
@@ -1,12 +1,12 @@
-########################################################################
-#                                                                      #
-#  Riccardo Soldini - riccardo.soldini@gmail.com                       #
-#                                                                      #
-#  unFold.py - 2020                                                    #
-#                                                                      #
-#  Modulo per la decostruzione di parti di carpenteria piegate         #
-#                                                                      #
-########################################################################
+######################################################
+#                                                                                                                             
+#  Riccardo Soldini - riccardo.soldini@gmail.com                                                      
+#                                                                                                                           
+#  unFold.py - 2020                                                                                               
+#                                                                                                                          
+#  Modulo per la decostruzione di parti di carpenteria piegate                                  
+#                                                                                                                         
+######################################################
 
 
 import math
@@ -33,9 +33,10 @@ OGG._getAnyBends()
 sheet_tree=FCTreeSheet(OGG)
 
 
-
+print (' ')
 for b in sheet_tree.Branches:
     bb=sheet_tree.Branches[b]
+    print ('faces:',bb.FaceUp,bb.FaceDown)
     print (b,bb.Class,bb.Joints)
     print (bb.FaceUp,bb.FaceDown)
     if bb.Class=='Cylinder':
@@ -46,9 +47,27 @@ doc=FC.newDocument()
 Gui.ActiveDocument=doc
 for b in sheet_tree.Branches:
     bb=sheet_tree.Branches[b]
-    f_up=OGG.Faces[bb.FaceUp].copy()
-    f_down=OGG.Faces[bb.FaceDown].copy()
-    Part.show(f_up)
-    Part.show(f_down)
+    if bb.Class=='Plane':
+        f_up=OGG.Faces[bb.FaceUp].copy()
+        f_down=OGG.Faces[bb.FaceDown].copy()
+        Part.show(f_up)
+        Part.show(f_down)
+    if bb.Class=='Cylinder':
+       p1=bb.PointOfRotation
+       p2=p1+bb.Axis
+       l = Part.LineSegment(p1,p2)
+       shape = l.toShape()
+       Part.show(shape)
+       ids = list(bb.Joints.keys()) 
+       print (ids)
+       j1=bb.Joints[ids[0]]
+       j2=bb.Joints[ids[1]]
+       print (ids[0],ids[1])
+       face1=OGG.Faces[j1.JoinUp[1]]
+       face2=OGG.Faces[j2.JoinUp[1]]
+       vns1 = face1.normalAt(0,0)
+       vns2 = face2.normalAt(0,0)
+       alpha = math.degrees( vns1.getAngle( vns2 ) )
+       print ('angle of bend:',alpha)     
        
 Gui.SendMsgToActiveView("ViewFit")
