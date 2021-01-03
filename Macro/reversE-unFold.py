03b995cf3568c54ba1836fc66b156c75bc02e711
soldocode <riccardo.soldini@gmail.com>
2021-01-03 20:02:28 +0100

    updated reversE-unFold.py

--- Macro/reversE-unFold.py
+++ Macro/reversE-unFold.py
@@ -32,14 +32,59 @@ def toDict(ts):
         vals['Angle']=branch.Angle
         vals['Axis']=branch.Axis
         vals['FaceUp']=branch.FaceUp
-        vals['FaceDown']=branch.FaceDown
+        vals['FaceDown']=branch.FaceDown      
+        vals['Joints']=[]
+        jj=list(branch.Joints)
+        for j in jj: 
+             vals['Joints'].append(branch.Joints[j].ToBranch.FaceUp)
         result[b]=vals
     return result 
 
+def linkedBranches(st,branch_id,id,b_list):
+    branch=st.Branches[branch_id]
+    print(branch,id)
+    jj=list(branch.Joints)
+    for j in jj:
+        look=branch.Joints[j].ToBranch.FaceUp
+        if id!=look:
+            if look not in b_list:b_list.append(look)
+            print(look)
+            linkedBranches(st,look,branch_id,b_list)
+    return b_list
 
-def unBend(bt,id_curve):
-      print ('unbend curve ',id_curve)
-      return
+def unBend(st,id_curve):
+    '''
+    input:
+         - st = sheetTree
+	 - id_curve = face id of curve to unbend
+    '''
+    msg=''
+    if id_curve in st.Branches:
+        if st.Branches[id_curve].Class=="Cylinder":
+            branch=st.Branches[id_curve]
+            msg='unbend curve  '+str(id_curve)
+            switch_list={'before':[],'after':[]}
+            jj=list(branch.Joints)
+            before_id=branch.Joints[jj[0]].ToBranch.FaceUp
+            after_id=branch.Joints[jj[1]].ToBranch.FaceUp
+            switch_list['before'].append(before_id)
+            switch_list['after'].append(after_id)
+            switch_list['before']+=linkedBranches(st,before_id,id_curve,[])
+            switch_list['after']+=linkedBranches(st,after_id,id_curve,[])
+            print(switch_list)
+            
+            axis=branch.Axis
+            pof=branch.PointOfRotation
+            angle=-branch.Angle
+            for o in switch_list['after']:
+                print (o)
+                obj=Part.Shape(OGG.Faces[o])
+                obj.Placement=rotateObj(obj,axis,pof,angle)
+                Part.show(obj)
+                       
+        else: msg='not a curve!'
+    else: msg='id not avaible!'
+    return msg
 
 
 print ("Let's begin...")
@@ -74,6 +119,8 @@ for b in sheet_tree.Branches:
         f_down=OGG.Faces[bb.FaceDown].copy()
         Part.show(f_up)
         Part.show(f_down)
+        #OGG.Faces[bb.FaceUp]=f_up
+        #OGG.Faces[bb.FaceDown]=f_down
     if bb.Class=='Cylinder':
        p1=bb.PointOfRotation
        p2=p1+bb.Axis
@@ -91,8 +138,12 @@ for b in sheet_tree.Branches:
        vns2 = face2.normalAt(0,0)
        alpha = math.degrees( vns1.getAngle( vns2 ) )
        print ('angle of bend:',alpha)   
-       f_up.Placement=rotateObj(f_up,bb.Axis,p1,90.0)
+       #f_up.Placement=rotateObj(f_up,bb.Axis,p1,90.0)
  
 pp.pprint(toDict(sheet_tree))     
-       
 Gui.SendMsgToActiveView("ViewFit")
+
+unBend(sheet_tree,5)
+unBend(sheet_tree,14)
+
+
